52f97e2b3d2b8550731bdd5ce3537350d1c3dcb4
Aymen Sakouhi <aymenmarketer@gmail.com>
2019-02-09 01:35:25 +0300

    add GET /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -6,7 +6,9 @@ const {user} = require('./../server/models/user');
 let id = '5c576869b38ec32628a7d382';
 
 /*let id = '5c5dea5126b0ed137ccebee4';
-
+if(!ObjectID.isValid(id)){
+    console.log("id not valid");
+}
 if(!ObjectID.isValid(id)){
     console.log("id not valid");
 }*/
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {user} = require('./models/user');
+
 
 let app = express();
 
@@ -30,7 +32,26 @@ app.get('/todos', (req, res) => {
 });
 
 
+//GET /todos/id
+app.get('/todos/:id',(req, res) => {
 
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+       res.status(404).send('not found at all!');
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+
+
+});
 
 
 
